c952dd9cb58d600982da1da88b4c47657bcc71a8
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2018-11-16 11:09:01 +0200

    the external scripts

---
+++ external_non_empty.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Safely produce a JSON object containing the result value.
+# jq will ensure that the value is properly quoted
+# and escaped to produce a valid JSON string.
+
+jq -n '{"value":"some_value"}'
