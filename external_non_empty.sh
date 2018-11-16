2f4103323abcae9a4e48d3406fa84592aa18c96c
qwerty1979bg <42143379+qwerty1979bg@users.noreply.github.com>
2018-11-16 12:29:35 +0200

    Update external_non_empty.sh

--- external_non_empty.sh
+++ external_non_empty.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Safely produce a JSON object containing the result value.
-# jq will ensure that the value is properly quoted
-# and escaped to produce a valid JSON string.
-
-jq -n '{"value":"some_value"}'
+echo '{'
+echo '"value": "some_value"'
+echo '}'
